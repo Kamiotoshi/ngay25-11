328cc49ccbad89088d2f1ff03bc5de6cd61b6642
Kamiotoshi <133650059+Kamiotoshi@users.noreply.github.com>
2023-11-27 21:01:22 +0700

    Add files via upload

---
+++ ngay25-11/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        News news = new News();
+        news.setTitle("Đánh giá");
+        news.setPublishDate("2023-11-23");
+        news.setAuthor("Minh");
+        news.setContent("Chất lương bài đọc");
+
+        // Nhập đánh giá và tính trung bình
+        news.inputRate();
+
+        // Hiển thị thông tin tin tức
+        news.Display();
+    }
+}
